6dbcbd71276fe64b8d78c36545a39c29329407a0
Obire John <youremail@gmail.com>
2026-06-13 06:03:59 -0700

    Initial commit

---
+++ config/agent.ts
@@ -0,0 +1,13 @@
+export const agent = {
+  name: "Adam Gothelf",
+  company: "Adams Rentals",
+  email: "rentals@adamgothelf.live",
+  phone: "(555) 555-5555",
+  domain: "adamgothelf.live",
+
+  title: "SOTHEBY'S INTERNATIONAL REALTY",
+
+  applicationFee: 54,
+
+  logoInitials: "AG",
+};
